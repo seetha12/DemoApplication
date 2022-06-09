69a42c2bbb720da6eaa2873ae5f1c83fa07e48b7
seetha.chakkapalli <imseetha12@gmail.com>
2022-06-09 19:16:34 +0530

    Consuming getUsers api

--- app/src/main/java/com/example/demoapplication/viewModel/SharedViewModel.kt
+++ app/src/main/java/com/example/demoapplication/viewModel/SharedViewModel.kt
@@ -2,8 +2,22 @@ package com.example.demoapplication.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.demoapplication.service.User
+import com.example.demoapplication.repository.UserRepository
+import com.example.demoapplication.service.Resource
+import com.example.demoapplication.service.model.User
 
-class SharedViewModel: ViewModel() {
-    val users: MutableLiveData<ArrayList<User>> = MutableLiveData<ArrayList<User>>()
+class SharedViewModel(private val userRepository: UserRepository): ViewModel() {
+    val users: MutableLiveData<Resource<ArrayList<User>>> = MutableLiveData<Resource<ArrayList<User>>>()
+
+    suspend fun getUsers() {
+        users.postValue(Resource.Loading())
+
+        val response = userRepository.getUsers()
+
+        if (response.isSuccessful && response.body() != null && !response.body()!!.data.isNullOrEmpty()) {
+            users.postValue(Resource.Success(response.body()?.data!!))
+        } else {
+            users.postValue(Resource.Error("Please try again..."))
+        }
+    }
 }
